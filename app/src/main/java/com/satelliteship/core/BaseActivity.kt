0a00fb2af85d132ecd5d5a67439d85c90a3430e4
ESRA KAYA <esrabayatiu@gmail.com>
2021-12-16 17:38:47 +0300

    Add core layer

---
+++ app/src/main/java/com/satelliteship/core/BaseActivity.kt
@@ -0,0 +1,5 @@
+package com.satelliteship.core
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity()
---
+++ app/src/main/java/com/satelliteship/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.satelliteship.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.satelliteship.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
